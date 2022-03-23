e820981137bde3c45365f14e29fe1ce87bc89449
mutia109 <mutiaraplg00@gmail.com>
2022-03-23 23:00:16 +0700

    commit component & props homework

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import data from '../../library/data';
 import './index.css';
 
-export default function Card() {
+export default function Card({image, title, artists}) {
   return (
     <div className='container'>
       <div className="card">
-        <img className="image" src={data.album.images[0].url} alt={data.album.name} />
-        <h3 className="title">{data.album.name}</h3>
-        <p className='text'>{data.album.artists[0].name}</p>
+        <img className="image" src={image} alt={title} />
+        <h3 className="title">{title}</h3>
+        <p className='artists'>{artists}</p>
         <div className="btn">
         <button id="select">Select</button>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Card from '../components/Card';
+import data from '../library/data';
 
 export default function Home() {
   return (
     <div className="home">
-      <Card/>
+      <Card 
+      image = {data.album.images[0].url}
+      title = {data.name}
+      artists ={data.artists[0].name}
+      />
     </div>
   );
 }
